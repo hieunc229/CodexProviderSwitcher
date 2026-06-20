c27e2be8ed2e27c398e6e1fdc7ea53fdede7e282
Hieu Nguyen (Jack) <nc.hieu@live.com>
2026-06-20 11:57:36 +0700

    Initial Commit

---
+++ CodexModelSwitcher/CodexModelSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexModelSwitcherApp.swift
+//  CodexModelSwitcher
+//
+//  Created by Hieu on 20/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexModelSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
